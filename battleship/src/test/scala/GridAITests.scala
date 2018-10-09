a2a9e83153e7fdd0ef532f233e126801a3979664
Unknown <Hugo.lecler@laposte.net>
2018-10-09 20:54:07 +0200

    unit tests added

--- battleship/src/test/scala/GridAITests.scala
+++ battleship/src/test/scala/GridAITests.scala
@@ -1,5 +1,18 @@
 import org.scalatest._
+import main.scala.model._
+import main.scala.business._
 
-class GridAITests extends FlatSpec with Matchers {
-
+class GridAITests extends FunSuite with DiagrammedAssertions {
+  test("The grid has had a boat added to it and shot") {
+    val humanGrid = GridHuman()
+    val humanGridBoat = humanGrid.setBoat(1,1)
+    val humanGridHit = humanGrid.setHit(1,1)
+    assert(humanGridHit.display(1)(1) == "3")
+  }
+  test("The grid has been shot and missed") {
+    val humanGrid = GridHuman()
+    val humanGridBoat = humanGrid.setBoat(1,1)
+    val humanGridHit = humanGrid.setMiss(0,1)
+    assert(humanGridHit.display(0)(1) == "2")
+  }
 }
--- battleship/src/test/scala/GridHumanTests.scala
+++ battleship/src/test/scala/GridHumanTests.scala
@@ -1,7 +1,19 @@
 
-
+import org.scalatest._
 import main.scala.model._
+import main.scala.business._
 
-class GridHumanTests {
-  val grid = GridHuman()
+class GridHumanTests extends FunSuite with DiagrammedAssertions {
+  test("The grid has had a boat added to it and shot") {
+    val aiGrid = GridAI()
+    val aiGridBoat = aiGrid.setBoat(1,1)
+    val aiGridHit = aiGridBoat.setHit(1,1)
+    assert(aiGridHit.display(1)(1) == "3")
+  }
+  test("The grid has been shot and missed") {
+    val aiGrid = GridAI()
+    val aiGridBoat = aiGrid.setBoat(1,1)
+    val aiGridHit = aiGridBoat.setMiss(0,1)
+    assert(humanGridHit.display(0)(1) == "2")
+  }
 }
